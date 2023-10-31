6d97ecf56209b7b861d7f3f0dd3d7ac1fff5c1da
Jacek Pietraszek <jacekpietraszek01@gmail.com>
2023-10-31 10:33:50 +0100

    Initial commit

---
+++ src/main/java/com/smartbudget/smartbudget/SmartBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.smartbudget.smartbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBudgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartBudgetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartbudget/smartbudget/SmartBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbudget.smartbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
