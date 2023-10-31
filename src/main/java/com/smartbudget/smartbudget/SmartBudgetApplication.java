ab39921191c0a8255c047d632c23ec39582af6c9
Jacek Pietraszek <jacekpietraszek01@gmail.com>
2023-10-31 11:20:10 +0100

    Creates Entity User and UserDto

--- src/main/java/com/smartbudget/smartbudget/SmartBudgetApplication.java
+++ src/main/java/com/smartbudget/smartbudget/SmartBudgetApplication.java
@@ -2,12 +2,14 @@ package com.smartbudget.smartbudget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SmartBudgetApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SmartBudgetApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SmartBudgetApplication.class, args);
+
     }
 
 }
---
+++ src/main/java/com/smartbudget/smartbudget/User.java
@@ -0,0 +1,87 @@
+package com.smartbudget.smartbudget;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String nick;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String nick, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.nick = nick;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", nick='" + nick + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
