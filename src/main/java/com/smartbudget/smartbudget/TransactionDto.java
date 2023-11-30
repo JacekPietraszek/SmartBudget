c9d57aa1d7a346e295c1dd65000d0f361b3ff5fc
Jacek Pietraszek <jacekpietraszek01@gmail.com>
2023-11-30 22:23:42 +0100

    Creates TransactionDto and Mapper

---
+++ src/main/java/com/smartbudget/smartbudget/TransactionDto.java
@@ -0,0 +1,60 @@
+package com.smartbudget.smartbudget;
+
+import java.time.LocalDateTime;
+
+public class TransactionDto {
+    private String TypeOfTransaction;
+    private String category;
+    private Double amount;
+    private String account;
+    private LocalDateTime dateAdded;
+    private String comments;
+
+    public String getTypeOfTransaction() {
+        return TypeOfTransaction;
+    }
+
+    public void setTypeOfTransaction(String typeOfTransaction) {
+        TypeOfTransaction = typeOfTransaction;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public LocalDateTime getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(LocalDateTime dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+}
---
+++ src/main/java/com/smartbudget/smartbudget/TransactionDtoMapper.java
@@ -0,0 +1,18 @@
+package com.smartbudget.smartbudget;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionDtoMapper {
+
+    TransactionDto map(Transaction transaction) {
+        TransactionDto dto = new TransactionDto();
+        dto.setTypeOfTransaction(transaction.getTypeOfTransaction());
+        dto.setCategory(transaction.getCategory());
+        dto.setAmount(transaction.getAmount());
+        dto.setAccount(transaction.getAccount());
+        dto.setDateAdded(transaction.getDateAdded());
+        dto.setComments(transaction.getComments());
+        return dto;
+    }
+}
