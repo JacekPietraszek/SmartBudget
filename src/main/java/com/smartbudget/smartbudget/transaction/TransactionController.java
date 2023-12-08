18a3d98281f838a0246a9b04a2e20ea9c505879b
Jacek Pietraszek <jacekpietraszek01@gmail.com>
2023-12-08 21:32:50 +0100

    Adds method getTransactionById

--- src/main/java/com/smartbudget/smartbudget/transaction/TransactionController.java
+++ src/main/java/com/smartbudget/smartbudget/transaction/TransactionController.java
@@ -2,6 +2,8 @@ package com.smartbudget.smartbudget.transaction;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,12 @@ public class TransactionController {
     public TransactionController(TransactionService transactionService, ObjectMapper objectMapper) {
         this.transactionService = transactionService;
         this.objectMapper = objectMapper;
+    }
+
+    ResponseEntity<TransactionDto> getTransactionById(@PathVariable Long id) {
+        return transactionService.getOfferById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
 
--- src/main/java/com/smartbudget/smartbudget/transaction/TransactionService.java
+++ src/main/java/com/smartbudget/smartbudget/transaction/TransactionService.java
@@ -3,6 +3,8 @@ package com.smartbudget.smartbudget.transaction;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class TransactionService {
     private TransactionRepository transactionRepository;
@@ -12,4 +14,10 @@ public class TransactionService {
         this.transactionRepository = transactionRepository;
         this.transactionDtoMapper = transactionDtoMapper;
     }
+
+    Optional<TransactionDto> getOfferById(Long id) {
+        return transactionRepository.findById(id).map(transactionDtoMapper::map);
+    }
+
+
 }
