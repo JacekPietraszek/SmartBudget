5b3840989a9e4bb5aa48ec72cc795137b1d6ca9e
Jacek Pietraszek <jacekpietraszek01@gmail.com>
2024-01-01 10:56:38 +0100

    Creates front for Transaction

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,21 @@
+function fetchAndDisplayTransactions() {
+    fetch('http://localhost:8080/transactions') // Upewnij się, że adres URL jest poprawny
+        .then(response => response.json())
+        .then(transactions => {
+            const table = document.getElementById('transactionsTable');
+            transactions.forEach(transaction => {
+                const row = table.insertRow();
+                row.insertCell(0).textContent = transaction.id;
+                row.insertCell(1).textContent = transaction.typeOfTransaction;
+                row.insertCell(2).textContent = transaction.category;
+                row.insertCell(3).textContent = transaction.value;
+                row.insertCell(4).textContent = transaction.account;
+                row.insertCell(5).textContent = transaction.dateAdded;
+                row.insertCell(6).textContent = transaction.comments;
+            });
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+// Wywołaj funkcję po załadowaniu strony
+document.addEventListener('DOMContentLoaded', fetchAndDisplayTransactions);
