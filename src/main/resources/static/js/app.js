1b9e3be334905e63a5b15c1ffa7f89e50451c80d
Jacek Pietraszek <jacekpietraszek01@gmail.com>
2024-01-07 22:48:41 +0100

    Adds filtr by category income and exponse

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -18,7 +18,9 @@ function fetchAndDisplayTransactions() {
 }
 
 function searchTransactions() {
-    var category = document.getElementById('searchInput').value;
+    var category = document.getElementById('categorySelect').value;
+    var incomeChecked = document.getElementById('income').checked;
+    var expenseChecked = document.getElementById('expense').checked;
 
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
@@ -30,23 +32,26 @@ function searchTransactions() {
 
                 const table = document.getElementById('transactionsTable');
                 transactions.forEach(transaction => {
-                    const row = table.insertRow();
-                    row.insertCell(0).textContent = transaction.id;
-                    row.insertCell(1).textContent = transaction.typeOfTransaction;
-                    row.insertCell(2).textContent = transaction.category;
-                    row.insertCell(3).textContent = transaction.value;
-                    row.insertCell(4).textContent = transaction.account;
-                    row.insertCell(5).textContent = transaction.dateAdded;
-                    row.insertCell(6).textContent = transaction.comments;
+                    if ((category === '' || transaction.category.toLowerCase() === category.toLowerCase()) &&
+                        (!incomeChecked || transaction.typeOfTransaction === 'income') &&
+                        (!expenseChecked || transaction.typeOfTransaction === 'expense')) {
+                        const row = table.insertRow();
+                        row.insertCell(0).textContent = transaction.id;
+                        row.insertCell(1).textContent = transaction.typeOfTransaction;
+                        row.insertCell(2).textContent = transaction.category;
+                        row.insertCell(3).textContent = transaction.value;
+                        row.insertCell(4).textContent = transaction.account;
+                        row.insertCell(5).textContent = transaction.dateAdded;
+                        row.insertCell(6).textContent = transaction.comments;
+                    }
                 });
             } else {
-
                 console.error(xhr.statusText);
             }
         }
     };
 
-    xhr.open('GET', '/transactions?category=' + encodeURIComponent(category), true);
+    xhr.open('GET', '/transactions', true);
     xhr.send();
 }
 
